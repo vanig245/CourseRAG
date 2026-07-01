63dd0858b9b8538dc5d61e6c792f02b047f03d07
vanig245 <vani.gupta2405@gmail.com>
2026-07-01 13:15:04 +0530

    extracted text page by page

--- docsprocess.py
+++ docsprocess.py
@@ -1,3 +1,13 @@
 from pypdf import PdfReader
-reader = PdfReader("AIML SYLLABUS.PDF")
-print(len(reader.pages))
+def loadpdf():
+    reader = PdfReader("AIML SYLLABUS.PDF")
+    print(f"Number of pages in pdf: {len(reader.pages)}")
+    if len(reader.pages) > 0:
+        page = reader.pages[0]
+        text = page.extract_text()
+        return text
+    else: 
+        print("pdf has no pages. Please try again!")
+        return None
+text = loadpdf()
+print(text)
