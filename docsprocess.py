e1360fdefb4b84cd5117f89732d2c63bf1981fe6
vanig245 <vani.gupta2405@gmail.com>
2026-07-01 15:47:16 +0530

    split text into chunks

--- docsprocess.py
+++ docsprocess.py
@@ -1,13 +1,17 @@
 from pypdf import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 def loadpdf():
     reader = PdfReader("AIML SYLLABUS.PDF")
     print(f"Number of pages in pdf: {len(reader.pages)}")
     if len(reader.pages) > 0:
-        page = reader.pages[0]
+        page = reader.pages[31]
         text = page.extract_text()
-        return text
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
+        texts = text_splitter.split_text(text)
+        return texts
     else: 
         print("pdf has no pages. Please try again!")
         return None
-text = loadpdf()
-print(text)
+    
+texts = loadpdf()
+print(texts)
