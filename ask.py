5807c5a5707c4c551891518457e295a7f600b757
vanig245 <vani.gupta2405@gmail.com>
2026-07-02 20:46:35 +0530

    created another file for user asking

---
+++ ask.py
@@ -0,0 +1,41 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+llm = ChatGroq(
+    model = "llama-3.1-8b-instant", 
+    api_key = os.environ.get("GROQ_API_KEY", "YOUR_GROQ_API_KEY_HERE") 
+)
+
+vector_db = Chroma(
+    persist_directory = "./knowledge_base",
+    embedding_function = embedding_model
+)
+
+question = input("Ask me anything :")
+retrieved_pdf = vector_db.similarity_search(question, k = 4)
+context = "\n\n".join([doc.page_content for doc in retrieved_pdf])
+
+print(context)
+
+prompt = f"""
+You are an AI teaching assistant. Answer the user's question based only on the following context from the syllabus.
+If the answer is not in the context, do not make things up. Just say "I don't know about this topic based on the provided syllabus".
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
+
+response = llm.invoke(prompt)
+print(response.content)
--- docsprocess.py
+++ docsprocess.py
@@ -1,22 +1,28 @@
 import os
 from pypdf import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_groq import ChatGroq
-from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 
 def loadpdf():
     reader = PdfReader("AIML SYLLABUS.PDF")
     print(f"Number of pages in pdf: {len(reader.pages)}")
 
-    chunk_texts = []
+    chunk_texts = [] 
+
     if len(reader.pages) > 0:
         for page in reader.pages:
             text = page.extract_text()
             if text:
-                text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
+                text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size = 1000,
+                    chunk_overlap = 200,
+                    length_function = len,
+                    is_separator_regex= False,
+                    )
                 texts = text_splitter.split_text(text)
                 chunk_texts.extend(texts)
+                # metadata.extend(texts)
                 
         return chunk_texts
 
